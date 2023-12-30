6c09d1bbf8511713381c0820be473284cd850ed8
Erfan Navab <erfannavab@gmail.com>
2023-12-30 11:25:00 +0330

    wrote a method for making the description less than 10 words

---
+++ src/main/java/org/example/ecommerce/util/Utils.java
@@ -0,0 +1,23 @@
+package org.example.ecommerce.util;
+
+public class Utils {
+
+    private Utils() {
+    }
+
+    public static String getLessDescription(String desc) {
+        String[] strs = desc.split(" ");
+
+        if (strs.length > 10) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                res.append(strs[i]);
+            }
+            return (res + "...");
+        } else {
+            return (desc + "...");
+        }
+
+    }
+
+}
